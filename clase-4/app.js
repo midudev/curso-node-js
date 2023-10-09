3e80ee27514be9df84cc93ab73398e71ef8522e8
Miguel Ángel Durán <miduga@gmail.com>
2023-10-09 17:16:45 +0200

    Remove line

--- clase-4/app.js
+++ clase-4/app.js
@@ -4,7 +4,6 @@ import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.use(json())
-app.use(corsMiddlewre())
 app.disable('x-powered-by')
 
 app.use('/movies', moviesRouter)
